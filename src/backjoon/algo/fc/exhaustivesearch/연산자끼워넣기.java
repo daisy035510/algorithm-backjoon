7f5297ec6cd403593a99b1a5216ebb622d099d94
daisy <43841944+daisy035510@users.noreply.github.com>
2023-08-24 14:31:42 +0900

    [완전탐색] 연산자끼워넣기

--- src/backjoon/algo/fc/exhaustivesearch/연산자끼워넣기.java
+++ src/backjoon/algo/fc/exhaustivesearch/연산자끼워넣기.java
@@ -4,62 +4,54 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class 연산자끼워넣기 {
-
     static FastReader sc = new FastReader();
     static StringBuilder sb = new StringBuilder();
-
+    static int N, min, max;
+    static int[] nums, operators;
     public static void main(String[] args) {
         input();
 
-        // 1 번째 원소부터 M 번째 원소를 조건에 맞게 고르는 모든 방법을 탐색해줘
         rec_func(1, nums[1]);
         sb.append(max).append('\n').append(min);
         System.out.println(sb.toString());
+
     }
 
-    static int N, max, min;
-    static int[] nums, operators, order;
-
-    public static void input() {
-
+    public static void input(){
         N = sc.nextInt();
         nums = new int[N + 1];
         operators = new int[5];
 
-        for (int i = 1; i <= N; i++) nums[i] = sc.nextInt();
-        for (int i = 1; i <= 4; i++) operators[i] = sc.nextInt();
+        for(int i = 1; i <= N; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        for(int i = 1; i <= 4; i++) {
+            operators[i] = sc.nextInt();
+        }
 
         min = Integer.MAX_VALUE;
         max = Integer.MIN_VALUE;
     }
 
-
-    // 피연산자 2개와 연산자가 주어졌을 때 계산해주는 함수
-    static int calculator(int operand1, int operator, int operand2){
-        System.out.println(" === operand1 : " + operand1 + ", operator : " + operator + ", operand2 : " + operand2);
-        // value, order[i], num[i+1]
-        if (operator == 1) // +
-            return operand1 + operand2;
-        else if (operator == 2) // -
-            return operand1 - operand2;
-        else if (operator == 3) // *
-            return operand1 * operand2;
-        else // /
-            return operand1 / operand2;
+    static int calculator(int operand1, int operator, int operand2) {
+        if(operator == 1) return operand1 + operand2;
+        else if(operator == 2) return operand1 - operand2;
+        else if(operator == 3) return operand1 * operand2;
+        else return operand1 / operand2;
     }
 
     static void rec_func(int k, int value) {
-        System.out.print("k : " + k + ", value : " + value);
-        if (k == N) {
-            // 완성된 식에 맞게 계산을 해서 정답에 갱신하는 작업
-            max = Math.max(max, value);
+
+        if(k == N) {
             min = Math.min(min, value);
+            max = Math.max(max, value);
         } else {
-            // k 번째 연산자는 무엇을 선택할 것인가?
-            for (int cand = 1; cand <= 4; cand++){
-                if (operators[cand] >= 1){
+            for(int cand = 1; cand <= 4; cand++) {
+
+                if(operators[cand] >= 1) {
                     operators[cand]--;
-                    rec_func(k + 1, calculator(value, cand, nums[k + 1]));
+                    rec_func(k+1, calculator(value, cand, nums[k+1]));
                     operators[cand]++;
                 }
             }
